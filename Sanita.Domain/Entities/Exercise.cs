6144d299ef5e00102d8b175dfd8b720b0acc93ea
Dawid Jaroszewicz <crispo9721@gmail.com>
2023-12-13 03:14:09 +0800

    ovveride SaveChangesAsync, fill variables on ChangeTracker state

---
+++ Sanita.Domain/Entities/Exercise.cs
@@ -0,0 +1,14 @@
+﻿using Sanita.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanita.Domain.Entities
+{
+    internal class Exercise : AuditableEntity
+    {
+        public string Name { get; set; }
+    }
+}
--- Sanita.Persistance/SanitaDbContext.cs
+++ Sanita.Persistance/SanitaDbContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sanita.Domain.Common;
 
 namespace Sanita.Persistance
 {
@@ -12,6 +13,40 @@ namespace Sanita.Persistance
         public SanitaDbContext(DbContextOptions<SanitaDbContext> options) : base(options)
         {               
                 
-        }       
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch(entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = string.Empty;
+                        entry.Entity.Created = DateTime.Now;
+                        entry.Entity.StatusId = 1;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.ModifiedBy = string.Empty;
+                        entry.Entity.Modified = DateTime.Now;
+                        break;
+                    case EntityState.Deleted:
+                        entry.Entity.ModifiedBy = string.Empty;
+                        entry.Entity.Modified = DateTime.Now;
+                        entry.Entity.Inactivated = DateTime.Now;
+                        entry.Entity.InactivatedBy = string.Empty;
+                        entry.Entity.StatusId = 0;
+                        entry.State = EntityState.Modified;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
