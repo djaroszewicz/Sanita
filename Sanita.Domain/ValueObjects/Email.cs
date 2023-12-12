65a8ef6b63375bada364f6119d3efaff5df6b885
Dawid Jaroszewicz <crispo9721@gmail.com>
2023-12-13 03:49:36 +0800

    create custom exception

---
+++ Sanita.Domain/ValueObjects/Email.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sanita.Domain.Common;
+using Sanita.Domain.Exceptions;
+
+namespace Sanita.Domain.ValueObjects
+{
+    public class Email : ValueObject
+    {
+        public string UserName { get; set; }
+        public string DomainName { get; set; }
+
+        public static Email For(string email)
+        {
+            var emailObj = new Email();
+            try
+            {
+                var index = email.IndexOf("@", StringComparison.Ordinal);
+                emailObj.UserName = email.Substring(0, index);
+                emailObj.DomainName = email.Substring(index + 1);
+            }
+            catch (Exception ex)
+            {
+                throw new EmailException(email, ex);
+            }
+
+            return emailObj;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
