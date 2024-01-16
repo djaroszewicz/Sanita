d117586422bea1e1a666b9387e8c42ef3f488b4b
Dawid Jaroszewicz <crispo9721@gmail.com>
2024-01-17 02:02:04 +0800

    Add Meal, Fridge, ExerciseType configuration

---
+++ Sanita.Persistance/Configurations/FridgeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sanita.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanita.Persistance.Configurations
+{
+    internal class FridgeConfiguration : IEntityTypeConfiguration<Fridge>
+    {
+        public void Configure(EntityTypeBuilder<Fridge> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.UserId).IsRequired();
+            builder.Property(p => p.Name).HasMaxLength(250).IsRequired();
+        }
+    }
+}
---
+++ Sanita.Persistance/Configurations/MealConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sanita.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanita.Persistance.Configurations
+{
+    internal class MealConfiguration : IEntityTypeConfiguration<Meal>
+    {
+        public void Configure(EntityTypeBuilder<Meal> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Description).HasMaxLength(1000);
+            builder.Property(p => p.Name).HasMaxLength(250).IsRequired();
+        }
+    }
+}
