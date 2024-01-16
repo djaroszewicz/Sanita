161e7ee8e8a32097a9108f6884ba604e49b63fd8
Dawid Jaroszewicz <crispo9721@gmail.com>
2024-01-17 01:55:56 +0800

    Add Exercise configuration

---
+++ Sanita.Persistance/Configurations/ExerciseConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sanita.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanita.Persistance.Configurations
+{
+    public class ExerciseConfiguration : IEntityTypeConfiguration<Exercise>
+    {
+        public void Configure(EntityTypeBuilder<Exercise> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name).HasMaxLength(250).IsRequired();
+            builder.Property(p => p.MovieUrl).HasMaxLength(80000);
+        }
+    }
+}
