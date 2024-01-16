b58db82791644e4a0c14edd63bf9c8b632415c28
Dawid Jaroszewicz <crispo9721@gmail.com>
2024-01-17 01:34:43 +0800

    BodyParameter configuration

--- Sanita.Domain/Entities/BodyParameter.cs
+++ Sanita.Domain/Entities/BodyParameter.cs
@@ -10,17 +10,17 @@ namespace Sanita.Domain.Entities
     public class BodyParameter : AuditableEntity
     {
         public int UserId { get; set; }
-        public decimal? Weigth { get; set; }
-        public decimal? Heigth { get; set; }
-        public decimal? Neck { get; set; }
-        public decimal? Chest { get; set; }
-        public decimal? Waist { get; set; }
-        public decimal? Stomach { get; set; }
-        public decimal? Hips { get; set; }
-        public decimal? Biceps { get; set; }
-        public decimal? Forearm { get; set; }
-        public decimal? Thigh { get; set; }
-        public decimal? Calf { get; set; }
+        public decimal Weigth { get; set; }
+        public decimal Heigth { get; set; }
+        public decimal Neck { get; set; }
+        public decimal Chest { get; set; }
+        public decimal Waist { get; set; }
+        public decimal Stomach { get; set; }
+        public decimal Hips { get; set; }
+        public decimal Biceps { get; set; }
+        public decimal Forearm { get; set; }
+        public decimal Thigh { get; set; }
+        public decimal Calf { get; set; }
 
     }
 }
--- Sanita.Persistance/Configurations/BodyParameterConfiguration.cs
+++ Sanita.Persistance/Configurations/BodyParameterConfiguration.cs
@@ -14,10 +14,18 @@ namespace Sanita.Persistance.Configurations
         public void Configure(EntityTypeBuilder<BodyParameter> builder)
         {
             builder.HasKey(p => p.Id);
-
-
-            builder.Property(p => p.Heigth).IsRequired();
-            builder.Property(p => p.Weigth).HasDefaultValue(0.0);
+            builder.Property(p => p.UserId).IsRequired();
+            builder.Property(p => p.Heigth).HasDefaultValue(0.00);
+            builder.Property(p => p.Weigth).HasDefaultValue(0.00);
+            builder.Property(p => p.Neck).HasDefaultValue(0.00);
+            builder.Property(p => p.Chest).HasDefaultValue(0.00);
+            builder.Property(p => p.Waist).HasDefaultValue(0.00);
+            builder.Property(p => p.Stomach).HasDefaultValue(0.00);
+            builder.Property(p => p.Hips).HasDefaultValue(0.00);
+            builder.Property(p => p.Biceps).HasDefaultValue(0.00);
+            builder.Property(p => p.Forearm).HasDefaultValue(0.00);
+            builder.Property(p => p.Thigh).HasDefaultValue(0.00);
+            builder.Property(p => p.Calf).HasDefaultValue(0.00);
         }
     }
 }
