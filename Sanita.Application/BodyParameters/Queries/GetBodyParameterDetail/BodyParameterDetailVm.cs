fe0d62f2bc26f94c3415c19cfe857149c3691837
Dawid_Jaroszewicz <Dawid.Jaroszewicz@dellteam.com>
2024-01-28 20:40:25 +0100

    add BodyParameterQueries

---
+++ Sanita.Application/BodyParameters/Queries/GetBodyParameterDetail/BodyParameterDetailVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanita.Application.BodyParameters.Queries.GetBodyParameterDetail
+{
+    public class BodyParameterDetailVm
+    {
+        public decimal Weigth { get; set; }
+        public decimal Heigth { get; set; }
+        public decimal Neck { get; set; }
+        public decimal Chest { get; set; }
+        public decimal Waist { get; set; }
+        public decimal Stomach { get; set; }
+        public decimal Hips { get; set; }
+        public decimal Biceps { get; set; }
+        public decimal Forearm { get; set; }
+        public decimal Thigh { get; set; }
+        public decimal Calf { get; set; }
+    }
+}
---
+++ Sanita.Application/BodyParameters/Queries/GetBodyParameterDetail/GetBodyParameterDetailsQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanita.Application.BodyParameters.Queries.GetBodyParameterDetail
+{
+    public class GetBodyParameterDetailsQueryHandler
+    {
+    }
+}
