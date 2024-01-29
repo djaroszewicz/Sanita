5125ccdbf3483ad052a51b23fcac6944726c68af
Dawid_Jaroszewicz <Dawid.Jaroszewicz@dellteam.com>
2024-01-29 10:03:28 +0100

    GetBodyParameterDetail

---
+++ Sanita.Application/BodyParameters/Queries/GetBodyParameterDetail/GetBodyParameterDetailQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Sanita.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanita.Application.BodyParameters.Queries.GetBodyParameterDetail
+{
+    public class GetBodyParameterDetailQueryHandler : IRequestHandler<GetBodyParameterDetailQuery, BodyParameterDetailVm>
+    {
+        private ISanitaDbContext _sanitaDbContext;
+        public GetBodyParameterDetailQueryHandler(ISanitaDbContext sanitaDbContext)
+        {
+            _sanitaDbContext = sanitaDbContext;
+        }
+        public async Task<BodyParameterDetailVm> Handle(GetBodyParameterDetailQuery request, CancellationToken cancellationToken)
+        {
+            var bodyParameter = await _sanitaDbContext.BodyParameters.Where(b => b.UserId == request.UserId).FirstOrDefaultAsync(cancellationToken);
+
+            var bodyParameterVm = new BodyParameterDetailVm
+            {
+                Weigth = bodyParameter.Weigth,
+                Heigth = bodyParameter.Heigth,
+                Neck = bodyParameter.Neck,
+                Chest = bodyParameter.Chest,
+                Waist = bodyParameter.Waist,
+                Stomach = bodyParameter.Stomach,
+                Hips = bodyParameter.Hips,
+                Biceps = bodyParameter.Biceps,
+                Forearm = bodyParameter.Forearm,
+                Thigh = bodyParameter.Thigh,
+                Calf = bodyParameter.Calf
+            };
+
+            return bodyParameterVm;
+        }
+    }
+}
