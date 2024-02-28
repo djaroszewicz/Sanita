1eaabf2eef506c0aa85f39bd05e891b6c3b546db
Dawid Jaroszewicz <crispo9721@gmail.com>
2024-02-29 03:39:50 +0800

    add PerformanceBehaviour

--- Sanita.Application/Common/Behaviours/LoggingBehaviour.cs
+++ Sanita.Application/Common/Behaviours/LoggingBehaviour.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
